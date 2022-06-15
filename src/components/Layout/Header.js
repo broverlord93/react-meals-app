790a67b01f29f53d7753f81c224e3bab8b114300
Anthony Limani <alimani9193@gmail.com>
2022-06-15 13:52:24 -0400

    Implement cart button component for header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import mealsImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
 
@@ -9,7 +10,7 @@ const Header = (props) => {
         <React.Fragment>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton></HeaderCartButton>
             </header>
             <div>
                 <img
