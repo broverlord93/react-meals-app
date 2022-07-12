c5b977d28fa875d670e9267793e9cd43fd7cb9b5
Anthony Limani <alimani9193@gmail.com>
2022-07-12 12:44:20 -0400

    Implements cartItemRemoveHandler() and cartItemAddHandler(), implements remove functionality to remove items from the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -26,8 +26,6 @@ const cartReducer = (state, action) => {
         } else {
             updatedItems = state.items.concat(action.item);
         }
-
-        // const updatedItems = state.items.concat(action.item);
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -35,7 +33,23 @@ const cartReducer = (state, action) => {
     }
 
     if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
 
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
     }
 
     return defaultCartState;
