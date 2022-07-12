5ad3f5d853f41b3cc9443c321caa2d489cca1037
Anthony Limani <alimani9193@gmail.com>
2022-07-12 11:27:09 -0400

    Fix a typo that was preventing Cart from properly updating on item-add.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,20 +7,22 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-    if (action.type === 'ADD'){
+    if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
 
-        const existingCartItemIndex = state.items.findIndex(item => item.id === item.action.id);
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
         const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
-        if (existingCartItem){
+        if (existingCartItem) {
             const updatedItem = {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount
             };
             updatedItems = [...state.items];
-            updatedItem[existingCartItemIndex] = updatedItem;
+            updatedItems[existingCartItemIndex] = updatedItem;
         } else {
             updatedItems = state.items.concat(action.item);
         }
@@ -32,7 +34,7 @@ const cartReducer = (state, action) => {
         };
     }
 
-    if (action.type === 'REMOVE'){
+    if (action.type === 'REMOVE') {
 
     }
 
