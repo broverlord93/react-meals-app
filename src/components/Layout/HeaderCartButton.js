3fff9fdf5a029f3c45d3d920228d4fe7486f07fe
Anthony Limani <alimani9193@gmail.com>
2022-07-12 12:55:21 -0400

    Adds bump-animation to the cart button in the header that activates when the cart is updated, using useEffect()

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 import CartContext from "../../store/cart-context";
 
@@ -6,12 +6,32 @@ import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHiglighted, setBtnIsHighlighted] = useState(false);
     const cartContext = useContext(CartContext);
 
     const numberOfCartItems = cartContext.items.reduce((curNumber, item) => curNumber += item.amount, 0);
 
+    const {items} = cartContext;
+
+    const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
